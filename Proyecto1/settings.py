093fce5c7f613ab40a23014a2adf2df6923deb12
JuanGuillermoJulioLeeSierraPoveda09 <juanguillermosierra2004@gmail.com>
2023-10-19 17:27:00 -0500

    Prototipo ID Searcher

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 current_directory = os.getcwd()
 direc=os.path.join(current_directory, "Proyecto1/plantillas/static")
 directo=os.path.join(current_directory, "Proyecto1/plantillas")
