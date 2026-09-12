a1a4a45cf1631ea9b2b1eb3bd4d1f30d019886b1
JuanGuillermoSierraPoveda <juanguillermosierra2004@gmail.com>
2026-09-12 07:51:36 -0500

    settings

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
      'ENGINE': 'django.db.backends.mysql',
         'NAME': 'srab',
         'USER': 'root',
-        'PASSWORD':'root',
+        'PASSWORD':'',
         'HOST': '127.0.0.1',
         'DATABASE_PORT':'3306',
     }
