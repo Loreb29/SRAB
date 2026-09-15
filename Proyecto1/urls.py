8aeff6733e85b47eaeb224f6da9b1bd3c7b74a82
JuanGuillermoSierraPoveda <juanguillermosierra2004@gmail.com>
2026-09-15 14:48:31 -0500

    Login webpage for admins done

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home, consultar_titulo
+from two_factor.views import LoginView
 
 urlpatterns = [
     path('', home, name='home'),
     path('api/consultar/', consultar_titulo, name='consultar_titulo'),
+    path('admin/login', LoginView.as_view(), name='login'),
 ]
